9327c9a1aa13999270f7d1aebb2018964269bdf2
vgoylo <y_draiv.by@mail.ru>
2022-01-17 23:53:10 +0300

    Add method create for salons && fix review comments

--- app/controllers/api/v1/salons_controller.rb
+++ app/controllers/api/v1/salons_controller.rb
@@ -1,13 +1,25 @@
 class Api::V1::SalonsController < ApplicationController
   def index
     salons = Salon.all
-    render json: salons, status: 200
+    render json: salons
   end
 
   def show
     salon = Salon.find_by(id: params[:id])
     if salon
-      render json: salon, status: 200
+      render json: salon
+    else
+      render json: { error: 'Error creating review.' }
+    end
+  end
+
+  def create
+    salon = Salon.new(
+      name: salon_params[:name],
+      address: salon_params[:address]
+    )
+    if salon.save!
+      render json: salon
     else
       render json: { error: 'Error creating review.' }
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :salons, only: [:index, :show]
+      resources :salons, only: [:index, :show, :create]
     end
   end
 end
