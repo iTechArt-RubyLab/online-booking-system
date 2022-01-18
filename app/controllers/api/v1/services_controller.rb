59fbd92b5c034351f785f8e0d136b7bdee1b167e
anton <tehanovanton@gmail.com>
2022-01-18 20:21:28 +0300

    Show action

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::ServicesController < ApplicationController
     services = Service.all
     render json: services
   end
+
+  def show
+    service = Service.find_by(id: params[:id])
+    
+    render json: service if service
+    render json: { message: 'service not found' }, status: 404 unless service
+  end
+
+  private
+
 end
