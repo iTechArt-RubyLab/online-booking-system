8b5baacc6fa6b6cd55a540dbbe16733d657ff2eb
valeriyapetrova <93676680+valeriyapetrova@users.noreply.github.com>
2022-01-18 18:01:15 +0300

    created table visits with base fields

---
+++ app/models/visit.rb
@@ -0,0 +1,7 @@
+class Visit < ApplicationRecord
+
+  enum status: { created: 0, approved: 1, rejected_by_user: 2, rejected_by_client: 3, deleted: 4, finished: 5 }
+
+  # belongs_to :client, foreign_key: :client_id
+  # belongs_to :salon, foreign_key: :salon_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_212511) do
+ActiveRecord::Schema.define(version: 2022_01_18_142004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 2022_01_18_212511) do
     t.integer "status", default: 0, null: false
     t.text "notes"
     t.string "image_url", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.datetime "start_at", null: false
+    t.datetime "end_at", null: false
+    t.integer "price", null: false
+    t.text "adress", null: false
+    t.integer "status", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
