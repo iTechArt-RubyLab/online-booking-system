879e26a355f00e28e3037622144901085b23675f
anton <tehanovanton@gmail.com>
2022-01-18 19:35:05 +0300

    Add Services_controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'services/index'
+  namespace :api do
+    namespace :v1 do
+      resources :services
+    end
+  end
 end
