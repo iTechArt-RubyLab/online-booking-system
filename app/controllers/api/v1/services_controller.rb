9ac2ede7271ecfdbc66f32b0b13e7e96b3c154e0
anton <tehanovanton@gmail.com>
2022-01-18 20:01:05 +0300

    Add validation

---
+++ app/controllers/api/v1/services_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::ServicesController < ApplicationController
+  def index
+    services = Service.all
+    render json: services
+  end
+end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,6 @@
 class Service < ApplicationRecord
+  validates :name, :description, :duration, :price, :hidden_price, :availability, presence: true
+  validates :name, uniqueness: true
+  validates :price, :hidden_price, :duration, numericality: { greater_than: 0 }
+  validates :duration, numericality: { less_than: 180 }
 end
