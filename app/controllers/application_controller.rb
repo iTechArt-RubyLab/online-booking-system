4c6fd2113ca0790ed7e8191708be8f8d447e8a7b
anton <tehanovanton@gmail.com>
2022-01-19 16:58:23 +0300

    Add record not found handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::ReordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found(exception)
+    render json: { error: exception.to_s }, status: :not_found
+  end
 end
