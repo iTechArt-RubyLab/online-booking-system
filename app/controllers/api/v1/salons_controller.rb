a96f50b56005f98c221da84bd583935f4863a8db
anton <tehanovanton@gmail.com>
2022-01-19 18:53:54 +0300

    Remove salon memoization

--- app/controllers/api/v1/salons_controller.rb
+++ app/controllers/api/v1/salons_controller.rb
@@ -1,14 +1,16 @@
 module Api
   module V1
     class SalonsController < ApplicationController
+      before_action :set_salon, only: %i[show update destroy] 
+
+      attr_accessor :salon
+
       def index
         @salons = Salon.all
         render json: @salons
       end
 
       def show
-        salon
-
         if salon
           render json: salon
         else
@@ -19,41 +21,38 @@ module Api
       def create
         @salon = Salon.new(salon_params)
 
-        if @salon.save!
-          render json: @salon
+        if salon.save!
+          render json: salon
         else
-          render json: { error: @salon.errors.full_messages }
+          render json: { error: salon.errors.full_messages }
         end
       end
 
       def update
-        salon
-
         if salon.update(salon_params)
           render json: { message: 'Salon was successfully updated' }
         else
-          render json: { error: 'Update not update salon' }
+          render json: { error: 'Update not update salon' }, status: :unprocessable_entity
         end
       end
 
       def destroy
-        salon
-
         if salon.destroy
           render json: { message: 'Salon was successfully updated' }
         else
-          render json: { error: 'Unable to delete salon' }
+          render json: { error: 'Unable to delete salon' }, status: :unprocessable_entity
         end
       end
 
-      def salon
-        @salon ||= Salon.find(params[:id])
+      private 
+
+      def set_salon
+        @salon = Salon.find(params[:id])
       end
 
       def salon_params
         params.require(:salon).permit(%i[name address phone email notes owner_id])
       end
-      private :salon, :salon_params
     end
   end
 end
