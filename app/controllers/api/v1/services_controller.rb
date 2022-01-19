062244b3dc8161357baa187167741084f2086f22
anton <tehanovanton@gmail.com>
2022-01-19 11:59:51 +0300

    Create action

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -11,6 +11,16 @@ class Api::V1::ServicesController < ApplicationController
     render json: { message: 'service not found' }, status: 404 unless service
   end
 
+  def create
+    service = Service.new(service_params)
+    
+    if service.save()
+      render json: service
+    else
+      render json: { message: service.errors.full_messages }, status: 400
+    end
+  end
+
   def destroy
     service = Service.find_by(id: params[:id])
     
@@ -19,5 +29,18 @@ class Api::V1::ServicesController < ApplicationController
     else
       render json: { message: 'service not found' }, status: 404
     end
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit([:category_id,
+                                     :salon_id,
+                                     :name,
+                                     :description,
+                                     :duration,
+                                     :price,
+                                     :hidden_price,
+                                     :availability])
   end
 end
