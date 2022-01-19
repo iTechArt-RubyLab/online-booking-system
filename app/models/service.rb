782c434dcbe4523b74f4fc1e0b74fae355a4bd1c
anton <tehanovanton@gmail.com>
2022-01-19 14:46:33 +0300

    Implement availability as eunm

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,4 +3,9 @@ class Service < ApplicationRecord
   validates :name, uniqueness: true
   validates :price, :hidden_price, numericality: { greater_than: 0 }
   validates :duration, numericality: { greater_than: 0, less_than_or_equal_to: 180 }
+
+  enum availability: {
+    availabile: 0,
+    not_availabile: 1
+  }
 end
