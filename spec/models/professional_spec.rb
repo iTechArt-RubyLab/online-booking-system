c425f3c839f8bba7c4a3296fff8a1992db91bb15
anton <tehanovanton@gmail.com>
2022-01-21 21:57:32 +0300

    Professional tests

--- spec/models/professional_spec.rb
+++ spec/models/professional_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
+RSpec.shared_examples "invalid professional" do |args|
+  subject { FactoryBot.build(:professional, args[:field]=>args[:value]) }
+
+  it { is_expected.to be_invalid }
+end
+
+
 RSpec.describe Professional, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe  'validation' do
+    context "presence with nil" do
+      include_examples 'invalid professional', { field: 'salon_id', value: nil }
+      include_examples 'invalid professional', { field: 'status', value: nil }
+      include_examples 'invalid professional', { field: 'work_email', value: nil }
+      include_examples 'invalid professional', { field: 'work_phone', value: nil }
+      include_examples 'invalid professional', { field: 'rating', value: nil }
+    end
+  
+    context "work email" do
+      context "uniqnuess" do
+        before { FactoryBot.create(:professional, work_email: 'something@gmail.com') }
+
+        include_examples 'invalid professional', { field: 'work_email', value: 'Something@gmail.com' }
+      end
+
+      context 'format' do
+        include_examples 'invalid professional', { field: 'work_email', value: 'Somethin.ggmail.com' }
+      end
+    end
+
+    context 'work_phone' do
+      include_examples 'invalid professional', { field: 'work_phone', value: '37 25 609-99-99' }
+    end
+  end
 end
