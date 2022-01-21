c8c6ae95b9a319b26e70ff20fdd22acb1634effc
anton <tehanovanton@gmail.com>
2022-01-21 13:43:40 +0300

    Move non-common validations to professional

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,3 +1,13 @@
 class Professional < User
   default_scope { where(role: :professional) }
+  
+  validates :salon_id, presence: true
+  validates :role, :status, presence: true
+  validates :image_url, presence: true, url: true
+  validates :work_email, presence: true,
+                         uniqueness: { case_sensitive: false },
+                         format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'Work email invalid' },
+                         length: { minimum: 4, maximum: 254 }  
+  validates :work_phone, presence: true,
+                         format: { with: /(\+375|80) (29|44|33|25) \d{3}-\d{2}-\d{2}/, message: 'Work phone invalid' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,31 +5,28 @@ class User < ApplicationRecord
   before_validation :capitalize_data, on: :create
   before_save :validate_notes
 
-  validates :first_name, presence: true, length: { minimum: 2, maximum: 255 }
-  validates :last_name, presence: true, length: { minimum: 2, maximum: 255 }
-  validates :patronymic, length: { maximum: 255 }, allow_blank: true
-  validates :salon_id, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false },
+  validates :first_name, presence: true,
+                         length: { minimum: 2, maximum: 255 }
+  validates :last_name, presence: true,
+                        length: { minimum: 2, maximum: 255 }
+  validates :patronymic, length: { maximum: 255 },
+                         allow_blank: true
+  validates :birthday, presence: true,
+                       date: { before: proc { Time.zone.today }, message: 'Birthday invalid' }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
                     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'Email invalid' },
                     length: { minimum: 4, maximum: 254 }
-  validates :work_email, presence: true, uniqueness: { case_sensitive: false },
-                         format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'Work email invalid' },
-                         length: { minimum: 4, maximum: 254 }
   validates :phone, presence: true,
-                    format: { with: /(\+375|80) (29|44|33|25) \d{3}-\d{2}-\d{2}/, message: 'Phone invalid' }
-  validates :work_phone, presence: true,
-                         format: { with: /(\+375|80) (29|44|33|25) \d{3}-\d{2}-\d{2}/, message: 'Work phone invalid' }
-  validates :birthday, presence: true, date: { before: proc { Time.zone.today }, message: 'Birthday invalid' }
-  validates :role, :status, presence: true
-  validates :image_url, presence: true, url: true
+                    format: { with: /(\+375|80) (29|44|33|25) \d{3}-\d{2}-\d{2}/, message: 'Phone invalid' }  
+
+  private
 
   def capitalize_data
     self.first_name = first_name.downcase.titleize
     self.last_name = last_name.downcase.titleize
     self.patronymic = patronymic.downcase.titleize if patronymic
   end
-
-  private
 
   def validate_notes
     self.notes = notes.chars.shuffle if notes.include?('</script>')
