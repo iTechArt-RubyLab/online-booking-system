3e94ff942f01d958c60905e10558bffa753b79d6
anton <tehanovanton@gmail.com>
2022-01-21 13:22:55 +0300

    Add rating field to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_101251) do
+ActiveRecord::Schema.define(version: 2022_01_21_102016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_01_21_101251) do
     t.string "image_url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
   end
 
   create_table "visits", force: :cascade do |t|
