771a940e117440c5f14465aae806872b98a59708
GEK62 <abasovwork@gmail.com>
2022-01-22 10:02:31 +0300

    models,routes,seed fix for tests

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,8 +5,9 @@ class Service < ApplicationRecord
   }
 
   validates :name, :description, :duration, :price, :hidden_price, :availability, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, length: { minimum: 2, maximum: 255 }
   validates :price, :hidden_price, numericality: { greater_than: 0 }
-  validates :duration, numericality: { greater_than: 0, less_than_or_equal_to: 180 }
+  validates :duration, numericality: { greater_than: 0, less_than_or_equal_to: 180, only_integer: true }
   validates :availability, inclusion: { in: Service.availabilities }
+  validates :description, length: { minimum: 10, maximum: 255 }
 end
