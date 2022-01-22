058e53f4111cd8af168901125dc50ae9251ef77b
GEK62 <abasovwork@gmail.com>
2022-01-22 06:25:52 +0300

    add tests

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'does not pass validations with' do
+    context 'nil name' do
+      subject { FactoryBot.build(:random_service, :nil_name) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'nil description' do
+      subject { FactoryBot.build(:random_service, :nil_description) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'nil duration' do
+      subject { FactoryBot.build(:random_service, :nil_duration) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'nil price' do
+      subject { FactoryBot.build(:random_service, :nil_price) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'nil hidden_price' do
+      subject { FactoryBot.build(:random_service, :nil_hidden_price) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'nil availability' do
+      subject { FactoryBot.build(:random_service, :nil_availability) }
+
+      it { is_expected.not_to be_valid }
+    end
+  end
 end
