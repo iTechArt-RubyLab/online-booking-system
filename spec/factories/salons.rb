20726e2f6bfed9196b695aee5498ff019e0b82ad
GEK62 <abasovwork@gmail.com>
2022-01-22 10:03:39 +0300

    salon tests

--- spec/factories/salons.rb
+++ spec/factories/salons.rb
@@ -1,10 +1,13 @@
 FactoryBot.define do
-  factory :random_salon do
+  factory :random_salon, class: 'Salon' do
     name { Faker::Company.name }
     address { Faker::Address.street_address }
-    phone { Faker::PhoneNumber.phone_number }
+    phone { '+375 29 123-45-67' }
     email { Faker::Internet.email }
     notes { Faker::Lorem.paragraph }
     owner_id { rand(1..100) }
   end
+
+  trait(:nil_owner_id) { owner_id { nil } }
+  trait(:nil_address) { address { nil } }
 end
--- spec/models/salon_spec.rb
+++ spec/models/salon_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Salon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(build(:random_salon)).to be_valid
+    end
+
+    context 'when nil name' do
+      subject { build(:random_salon, :nil_name) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'when nil address' do
+      subject { build(:random_salon, :nil_address) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'when nil phone' do
+      subject { build(:random_salon, :nil_phone) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'when nil email' do
+      subject { build(:random_salon, :nil_email) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'when nil notes' do
+      subject { build(:random_salon, :nil_notes) }
+
+      it { is_expected.to be_valid }
+    end
+  end
 end
