97dfad2be615d5e789f24e384348c3b1067e0531
anton <tehanovanton@gmail.com>
2022-01-24 18:40:13 +0300

    Rubocop fixes

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -3,27 +3,27 @@ require 'rails_helper'
 RSpec.describe Service, type: :model do
   describe 'does not pass validations' do
     context 'when name nil' do
-      it { should validate_presence_of :name }
+      it { is_expected.to validate_presence_of :name }
     end
 
     context 'when description nil' do
-      it { should validate_presence_of :description }
+      it { is_expected.to validate_presence_of :description }
     end
 
     context 'when duration nil' do
-      it { should validate_presence_of :duration }
+      it { is_expected.to validate_presence_of :duration }
     end
 
     context 'when price nil' do
-      it { should validate_presence_of :price }
+      it { is_expected.to validate_presence_of :price }
     end
 
     context 'when hidden_price nil' do
-      it { should validate_presence_of :hidden_price }
+      it { is_expected.to validate_presence_of :hidden_price }
     end
 
     context 'when availability nil' do
-      it { should validate_presence_of :availability }
+      it { is_expected.to validate_presence_of :availability }
     end
   end
 end
