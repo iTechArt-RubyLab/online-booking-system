a4ba31cefe22e752ff7b1fad71fe6fdb9bae1edd
anton <tehanovanton@gmail.com>
2022-01-25 13:36:38 +0300

    rubocop fixes

--- spec/controlllers/api/v1/visits_controller_spec.rb
+++ spec/controlllers/api/v1/visits_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
-RSpec.describe Api::V1::VisitsController, :type => :controller do
-  describe "GET #index" do
+RSpec.describe Api::V1::VisitsController, type: :controller do
+  describe 'GET #index' do
     before do
       get :index
     end
-    it "JSON body response contains expected attributes" do
-      json_response = JSON.parse(response.body)
-      expect(hash_body.keys).to match_array([:start_at, :end_at, :price, :adress, :status])
+
+    it 'JSON body response contains expected attributes' do
+      expect(hash_body.keys).to match_array(%i[start_at end_at price adress status])
     end
   end
 end
