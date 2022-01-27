d34280b9a40def8b2a3cf26594ec146ebd39387a
vgoylo <y_draiv.by@mail.ru>
2022-01-27 15:18:08 +0300

    Refacnjring

--- app/models/salon_owner.rb
+++ app/models/salon_owner.rb
@@ -1,7 +1,5 @@
 class SalonOwner < User
   default_scope { where(role: :salon_owner) }
 
-  validates :work_email, :work_phone, :status, presence: true
-
   has_many :salons, dependent: :destroy
 end
